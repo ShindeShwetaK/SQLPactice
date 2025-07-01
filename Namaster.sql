29bf72cf19be7e9403e60737de133107bae2557a
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2025-07-01 10:10:27 -0700

    Update Namaster.sql

--- Namaster.sql
+++ Namaster.sql
@@ -404,7 +404,29 @@ ORDER BY project_count DESC;
 ########################################################################################################################
 
 ########################################################################################################################
+17 - Business Expansion
+  Amazon is expanding their pharmacy business to new cities every year. You are given a table of business operations where 
+  you have information about cities where Amazon is doing operations along with the business date information.
 
+Write a SQL to find year wise number of new cities added to the business, display the output in increasing order of year.
+
+Table: business_operations
++---------------+-----------+
+| COLUMN_NAME   | DATA_TYPE |
++---------------+-----------+
+| business_date | date      |
+| city_id       | int       |
++---------------+-----------+
+  Ans:
+  with cte as (
+select city_id,min(YEAR(business_date)) as first_operation_year
+ from business_operations 
+ group by city_id
+)
+select first_operation_year,count(*) as no_of_new_cities
+from cte
+group by first_operation_year
+ORDER BY first_operation_year ASC;
 ########################################################################################################################
 
 ########################################################################################################################
